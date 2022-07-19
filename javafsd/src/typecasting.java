4ce957a903bfaed3c48fed717395bd6158ccd419
mayuribenal <mayuribenal111@gmail.com>
2022-07-20 01:40:29 +0530

    program

---
+++ javafsd/src/typecasting.java
@@ -0,0 +1,38 @@
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Implicit Type Casting");
+		char b='B';
+		System.out.println("Value of b: "+b);
+		int k=b;
+		System.out.println("Value of b: "+b);
+		
+		float c=b;
+		System.out.println("Value of c: "+c);
+		
+		long d=b;
+		System.out.println("Value of d: "+d);
+		
+		double e=b;
+		System.out.println("Value of e: "+e);
+		
+				
+		System.out.println("\n");
+		
+		System.out.println("Explicit Type Casting");
+		//explicit conversion
+		
+		double m=65.5;
+		int n=(int)m;
+		System.out.println("Value of m: "+m);
+		System.out.println("Value of n: "+n);
+		
+	
+
+
+	}
+
+}
